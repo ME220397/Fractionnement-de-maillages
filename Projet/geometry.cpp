78c5d062eadcd73447a0d176dead0d540e3ecf38
leokamm <leokammerlocher.ds@gmail.com>
2021-01-12 16:44:55 +0100

    Implementation methode mediator_plan/is_intersected de la classe Geometry

--- Projet/geometry.cpp
+++ Projet/geometry.cpp
@@ -1,4 +1,5 @@
 #include "geometry.h"
+#include <QMatrix4x4>
 
 Geometry::Geometry()
 {
@@ -17,14 +18,59 @@ MyMesh::Point Geometry::get_midpoint(MyMesh::Point A, MyMesh::Point B)
 
 MyMesh::Point Geometry::get_vect(MyMesh::Point A, MyMesh::Point B)
 {
-    MyMesh::Point AB;
+    MyMesh::Point AB;//Le vecteur allant de A vers B
     AB[0] = B[0]-A[0];
     AB[1] = B[1]-A[1];
     AB[2] = B[2]-A[2];
     return AB;
 }
 
-std::vector Geometry::get_mediator_plan(MyMesh::Point A, MyMesh::Point B)
+std::vector<MyMesh::Point> Geometry::get_mediator_plan(MyMesh::Point A, MyMesh::Point B)
 {
+    MyMesh::Point I; //Le milieu de AB
+    I = get_midpoint(A, B);
 
+    MyMesh::Point AB; //Le vecteur allant de A vers B
+    AB = get_vect(A, B);
+
+    MyMesh::Point U; //Le premier vecteur du plan mediateur
+    MyMesh::Point V; //Le second vecteur du plan mediateur
+
+    QMatrix4x4 Rx; //Creation de la matrice de rotation autour de l'axe x de 90 degres
+    Rx.setToIdentity();
+    Rx.rotate(90, QVector3D(1, 0, 0));
+
+    QMatrix4x4 Ry; //Creation de la matrice de rotation autour de l'axe y de 90 degres
+    Ry.setToIdentity();
+    Ry.rotate(90, QVector3D(0, 1, 0));
+
+    U = AB * Rx;
+    V = AB * Ry;
+
+    std::vector<MyMesh::Point> med_plan;
+    med_plan.pushback(I);
+    med_plan.pushback(U);
+    med_plan.pushback(V);
+    med_plan.pushback(AB) // On garde la normale au plan pour vérifier plus simplement si deux plans s'intersectent
+    return med_plan;
 }
+
+bool Geometry::is_intersected(std::vector<MyMesh::Point> P, std::vector<MyMesh::Point> Q)
+{
+    if(P.at(3)[0]/Q.at(3)[0] == P.at(3)[1]/Q.at(3)[1] && P.at(3)[0]/Q.at(3)[0] == P.at(3)[2]/Q.at(3)[2])
+    {
+        return false;
+    }
+    return true;
+}
+
+
+
+
+
+
+
+
+
+
+
--- Projet/geometry.h
+++ Projet/geometry.h
@@ -9,7 +9,8 @@ public:
     Geometry();
     MyMesh::Point get_midpoint(MyMesh::Point A, MyMesh::Point B); //Recupere le milieur de AB
     MyMesh::Point get_vect(MyMesh::Point A, MyMesh::Point B); //Recupere le vecteur AB
-    std::vector get_mediator_plan(MyMesh::Point A, MyMesh::Point B); //Recupere le plan mediateur du vecteur AB
+    std::vector<MyMesh::Point> get_mediator_plan(MyMesh::Point A, MyMesh::Point B); //Recupere le plan mediateur du vecteur AB
+    bool is_intersected(std::vector<MyMeshPoint> P, std::vector<MyMesh::Point> Q);
 };
 
 #endif // GEOMETRY_H
